599b31d969d4613a751e7b54c0b9345bd30675c6
KANAN-DAVE <kanandave28@gmail.com>
2021-12-22 19:20:07 +0530

    commited by : kanan

--- hello.c
+++ hello.c
@@ -1,4 +1,8 @@
-#include<stdio.h>
-void main() {
-    printf("Hello Beautiful World!!");
+#include<stdio.h>>
+#include<conio.h>
+int main(){
+    printf("hello word");
+    printf("how are you?");
+    printf("changed done by kanan");
+    return 0;
 }
